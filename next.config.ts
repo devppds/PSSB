1894bedb9a94098cb6abfba5fe8c9086352d2871
develzy <ppdslirboyo@gmai.com>
2026-01-07 18:37:44 +0700

    Attempt to fix Error 1101: Updated compatibility date, config, and bundling

--- next.config.ts
+++ next.config.ts
@@ -1,7 +1,7 @@
 import type { NextConfig } from "next";
 import { initOpenNextCloudflareForDev } from "@opennextjs/cloudflare/cloudflare-context";
 
-initOpenNextCloudflareForDev();
+// initOpenNextCloudflareForDev();
 
 const nextConfig: NextConfig = {
   serverExternalPackages: ['async_hooks', 'fs', 'path', 'node:async_hooks', 'node:fs', 'node:path', 'node:dns', 'node:net']

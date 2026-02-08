650c7f461e21cbd94754e04315a763fbb543f558
devppds <ppdslirboyo@gmail.com>
2026-02-08 16:09:16 +0700

    fix: add cloudinary and google to next.config.ts image remotePatterns

--- next.config.ts
+++ next.config.ts
@@ -15,6 +15,22 @@ const nextConfig: NextConfig = {
   typescript: {
     ignoreBuildErrors: true,
   },
+  images: {
+    remotePatterns: [
+      {
+        protocol: 'https',
+        hostname: 'res.cloudinary.com',
+        port: '',
+        pathname: '/**',
+      },
+      {
+        protocol: 'https',
+        hostname: 'lh3.googleusercontent.com',
+        port: '',
+        pathname: '/**',
+      }
+    ],
+  },
 };
 
 export default nextConfig;
